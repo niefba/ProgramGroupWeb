98915744cd95fa8b1a50e54e5754f48cf20dc1fe
fabien <fabien.tillon@rupil.fr>
2016-03-16 20:05:11 +0100

    Fichier de config

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 var config = {};
 
+// Config for server
 config.server = {};
 config.server.port = 3000;
 
+// Config for database
 config.couchebase = {};
 config.couchebase.host = "localhost";
 
+// Config for application
+config.app = {};
+config.app.secret = "616161";
+
+// Export config
 module.exports = config;
--- server.js
+++ server.js
@@ -2,19 +2,16 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express')
   , util = require('util')
   , app = require('express')()
   , server = require('http').createServer(app)
-//  , io = require('socket.io').listen(server)
   , jwt = require('jwt-simple');
 
-var secret = "616161";
-//var body = {};
+var config = require('./config');
 
 // Start server
-server.listen(8888, function(){
+server.listen(config.server.port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
@@ -26,7 +23,6 @@ app.use(function(req, res, next) {
 });
 app.use('/angular', express.static(__dirname + '/node_modules/angular'));
 app.use('/angular-route', express.static(__dirname + '/node_modules/angular-route'));
-// app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io'));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap'));
 app.use('/css', express.static(__dirname + '/public/css'));
@@ -40,23 +36,13 @@ app.get('/token/:key', function(req, res, next) {
 });
 
 app.get('/data', function(req, res){
-  /*
-  if (body[req.query.token]) {
-    res.status(200).json(body[req.query.token]);
-    delete body[req.query.token];
-  }
-  else {
-    res.status(403).json({ error: 'The data has expired'});
-  }
-  */
   try {
     // Decode the json from an encoded string with double quote
-    var data = require('jwt-simple').decode(req.query.token, secret, "HS256");
-    console.dir(data);
+    var data = require('jwt-simple').decode(req.query.token, config.app.secret, "HS256");
     res.status(200).json(data);
   }
   catch (err) {
-    console.log(err);
+    res.status(403).json({ error: 'Invalid token'});
   }
 });
 
